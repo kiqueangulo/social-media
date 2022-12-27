bf62842c41f3b793f06b4438fd29a7463744c435
kiqueangulo <kiqueangulo@gmail.com>
2022-12-27 17:15:42 -0500

    Hash password and create auth token
    There was a bug caused by a typo in the User model

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,11 +1,46 @@
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+const { SECRET_KEY } = require("../../config")
 const User = require("../../models/User")
 
 module.exports = {
   Mutation: {
-    register(_, args, context, info) {
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } },
+      context,
+      info
+    ) {
       // TODO: Validate user data
       // TODO: Make sure user doesn't already exist
       // TODO: Hash password and create auth token
+      password = await bcrypt.hash(password, 12)
+
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      })
+
+      const result = await newUser.save()
+
+      const token = jwt.sign(
+        {
+          id: result.id,
+          email: result.email,
+          username: result.username,
+        },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      )
+
+      return {
+        ...result._doc,
+        id: result.id,
+        token,
+      }
     },
   },
 }
