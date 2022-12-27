1b6cc876ecc3bd67973e8a20a6ffe57ca973ed02
kiqueangulo <kiqueangulo@gmail.com>
2022-12-27 16:02:54 -0500

    Retrieve posts from the database

--- index.js
+++ index.js
@@ -2,17 +2,33 @@ const { ApolloServer } = require("apollo-server")
 const gql = require("graphql-tag")
 const mongoose = require("mongoose")
 
+const Post = require("./models/Post.js")
 const { MONGODB_URI } = require("./config.js")
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `
 
 const resolvers = {
   Query: {
-    sayHi: () => "Hello World!",
+    async getPosts() {
+      try {
+        const posts = await Post.find()
+
+        return posts
+      } catch (error) {
+        throw new Error(error)
+      }
+    },
   },
 }
 
