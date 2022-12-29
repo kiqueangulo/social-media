9c56182b6803158dc4a1fec2a535958788759a5a
kiqueangulo <kiqueangulo@gmail.com>
2022-12-28 19:33:09 -0500

    Create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+
+import "semantic-ui-css/semantic.min.css"
 import "./App.css"
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <Route exact path="/" element={Home} />
+      <Route exact path="/login" element={Login} />
+      <Route exact path="/register" element={Register} />
+    </Router>
   )
 }
 
