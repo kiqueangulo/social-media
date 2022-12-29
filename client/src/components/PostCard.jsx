273197e7300f1511280949e8de3ab659281a9f9d
kiqueangulo <kiqueangulo@gmail.com>
2022-12-28 23:57:34 -0500

    PostCard structured

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,8 +1,34 @@
-function PostCard() {
+import moment from "moment"
+
+function PostCard({ post: { username, body, createdAt, likeCount, commentCount } }) {
+  const likePost = () => console.log("Post liked!")
+
+  const commentOnPost = () => console.log("Comment on post!")
+
   return (
-    <div>
-      <h1>PostCard</h1>
-    </div>
+    <>
+      <img
+        src="https://thumbs.dreamstime.com/b/l%C3%ADnea-icono-del-negro-avatar-perfil-de-usuario-121102131.jpg"
+        alt="Generic avatar icon"
+        width="100px"
+      />
+
+      <h3>{username}</h3>
+      <p>{moment(createdAt).fromNow(true)}</p>
+
+      <p>{body}</p>
+
+      <div>
+        <div>
+          <button onClick={likePost}>🤍</button>
+          <span>{likeCount}</span>
+        </div>
+        <div>
+          <button onClick={commentOnPost}>💬</button>
+          <span>{commentCount}</span>
+        </div>
+      </div>
+    </>
   )
 }
 
