3f65ac3f836f390a30318249fc58ff6a393fa6cd
kiqueangulo <kiqueangulo@gmail.com>
2022-12-28 22:57:21 -0500

    Make the first query on client side
    The dependency apollo-client was giving problems and I changed it for @apollo/client

--- client/src/ApolloProvider.jsx
+++ client/src/ApolloProvider.jsx
@@ -1,6 +1,7 @@
-import ApolloClient from "apollo-client"
+// import ApolloClient from "apollo-client"
+import { ApolloClient } from "@apollo/client"
 import { InMemoryCache } from "apollo-cache-inmemory"
-import { createHttpLink } from "apollo-link-http"
+import { createHttpLink } from "@apollo/react-hooks"
 import { ApolloProvider } from "@apollo/react-hooks"
 
 import App from "./App"
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,29 @@
-import React from "react"
+import { useQuery } from "@apollo/react-hooks"
+import gql from "graphql-tag"
+
+const FETCH_POSTS = gql`
+  query Posts {
+    getPosts {
+      id
+      username
+      body
+      createdAt
+    }
+  }
+`
 
 function Home() {
-  return <div>Home</div>
+  const { loading, data } = useQuery(FETCH_POSTS)
+
+  if (data) {
+    console.log(data)
+  }
+
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
 }
 
 export default Home
