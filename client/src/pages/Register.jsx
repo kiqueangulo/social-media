a2a66eea5c63c4977e33d5ab6a92830b96eb8264
kiqueangulo <kiqueangulo@gmail.com>
2023-01-03 15:12:10 -0500

    Cretae register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,73 @@
-import React from "react"
+import { useState } from "react"
 
 function Register() {
-  return <div>Register</div>
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  })
+
+  const changeValue = e => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDafault()
+
+    // GraphQL mutation here
+  }
+
+  return (
+    <div>
+      <form action="#" onSubmit={handleSubmit} noValidate>
+        <h1>Register</h1>
+
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={values.username}
+            onChange={changeValue}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={values.email}
+            onChange={changeValue}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={values.password}
+            onChange={changeValue}
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            value={values.confirmPassword}
+            onChange={changeValue}
+          />
+        </div>
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  )
 }
 
 export default Register
