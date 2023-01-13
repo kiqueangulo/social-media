b1919d54ae5a0e17025ce404fa4f498ed777bf72
kiqueangulo <kiqueangulo@gmail.com>
2023-01-13 15:56:11 -0500

    Leave the AuthRoute for TODO list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 import { AuthProvider } from "./context/auth"
-import AuthRoute from "./utils/AuthRoute"
+// import AuthRoute from "./utils/AuthRoute"
 
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
@@ -18,8 +18,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <AuthRoute exact path="/login" element={<Login />} />
-          <AuthRoute exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
         </Routes>
       </Router>
     </AuthProvider>
--- client/src/utils/AuthRoute.js
+++ client/src/utils/AuthRoute.js
@@ -3,6 +3,7 @@ import { Route, Navigate } from "react-router-dom"
 
 import { AuthContext } from "../context/auth"
 
+// TODO: Figure out why this security component does not work
 function AuthRoute({ element: Component, ...rest }) {
   const { user } = useContext(AuthContext)
 
